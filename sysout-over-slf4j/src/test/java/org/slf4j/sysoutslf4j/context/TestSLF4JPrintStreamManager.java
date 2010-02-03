a17efdd1b26666a140655895941b99377035dead
Robert Elliot <rob@rob.id.bskyb.com>
2010-02-03 15:31:55 +0000

    Testing work...

---
+++ sysout-over-slf4j/src/test/java/org/slf4j/sysoutslf4j/context/TestSLF4JPrintStreamManager.java
@@ -0,0 +1,11 @@
+package org.slf4j.sysoutslf4j.context;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rob
+ * Date: 03-Feb-2010
+ * Time: 08:36:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestSLF4JPrintStreamManager {
+}
