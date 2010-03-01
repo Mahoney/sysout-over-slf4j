b515a33a691dc42a6eee8e589809f78edac1a230
Robert <Robert@192.168.1.3>
2010-03-01 23:28:33 +0000

    Boosting test coverage

--- sysout-over-slf4j/src/test/java/org/slf4j/sysoutslf4j/context/TestLogLevel.java
+++ sysout-over-slf4j/src/test/java/org/slf4j/sysoutslf4j/context/TestLogLevel.java
@@ -3,7 +3,9 @@ package org.slf4j.sysoutslf4j.context;
 import static org.easymock.EasyMock.createStrictMock;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.Marker;
@@ -12,13 +14,18 @@ import org.slf4j.helpers.BasicMarkerFactory;
 public class TestLogLevel {
 	
 	private Logger mockLogger = createStrictMock(Logger.class);
+	private Marker expectedMarker = makeMarker();
+	
+	@After
+	public void verifyMocks() {
+		verify(mockLogger);
+	}
 	
 	@Test
 	public void testLogStringLevelTraceDelegatesToLogger() {
 		mockLogger.trace("expected");
 		replay(mockLogger);
 		LogLevel.TRACE.log(mockLogger, "expected");
-		verify(mockLogger);
 	}
 	
 	@Test
@@ -26,7 +33,6 @@ public class TestLogLevel {
 		mockLogger.debug("expected");
 		replay(mockLogger);
 		LogLevel.DEBUG.log(mockLogger, "expected");
-		verify(mockLogger);
 	}
 	
 	@Test
@@ -34,7 +40,6 @@ public class TestLogLevel {
 		mockLogger.info("expected");
 		replay(mockLogger);
 		LogLevel.INFO.log(mockLogger, "expected");
-		verify(mockLogger);
 	}
 	
 	@Test
@@ -42,7 +47,6 @@ public class TestLogLevel {
 		mockLogger.warn("expected");
 		replay(mockLogger);
 		LogLevel.WARN.log(mockLogger, "expected");
-		verify(mockLogger);
 	}
 	
 	@Test
@@ -50,16 +54,47 @@ public class TestLogLevel {
 		mockLogger.error("expected");
 		replay(mockLogger);
 		LogLevel.ERROR.log(mockLogger, "expected");
-		verify(mockLogger);
 	}
 	
 	@Test
-	public void testLogMarkerStringDelegatesToLogger() {
-		Marker expectedMarker = makeMarker();
+	public void testLogMarkerStringLevelTraceDelegatesToLogger() {
+		mockLogger.trace(expectedMarker, "expected");
+		replay(mockLogger);
+		LogLevel.TRACE.log(mockLogger, expectedMarker, "expected");
+	}
+	
+	@Test
+	public void testLogMarkerStringLevelDebugDelegatesToLogger() {
+		mockLogger.debug(expectedMarker, "expected");
+		replay(mockLogger);
+		LogLevel.DEBUG.log(mockLogger, expectedMarker, "expected");
+	}
+	
+	@Test
+	public void testLogMarkerStringLevelInfoDelegatesToLogger() {
+		mockLogger.info(expectedMarker, "expected");
+		replay(mockLogger);
+		LogLevel.INFO.log(mockLogger, expectedMarker, "expected");
+	}
+	
+	@Test
+	public void testLogMarkerStringLevelWarnDelegatesToLogger() {
+		mockLogger.warn(expectedMarker, "expected");
+		replay(mockLogger);
+		LogLevel.WARN.log(mockLogger, expectedMarker, "expected");
+	}
+	
+	@Test
+	public void testLogMarkerStringLevelErrorDelegatesToLogger() {
 		mockLogger.error(expectedMarker, "expected");
 		replay(mockLogger);
 		LogLevel.ERROR.log(mockLogger, expectedMarker, "expected");
-		verify(mockLogger);
+	}
+	
+	@Test
+	public void valueOf() {
+		assertEquals(LogLevel.WARN, LogLevel.valueOf("WARN"));
+		replay(mockLogger);
 	}
 
 	private Marker makeMarker() {
--- sysout-over-slf4j/src/test/java/org/slf4j/sysoutslf4j/context/TestReferenceHolder.java
+++ sysout-over-slf4j/src/test/java/org/slf4j/sysoutslf4j/context/TestReferenceHolder.java
@@ -1,10 +1,25 @@
 package org.slf4j.sysoutslf4j.context;
 
+import static org.junit.Assert.assertNotNull;
 import static org.slf4j.testutils.Assert.assertNotInstantiable;
+
+import java.lang.ref.WeakReference;
 
 import org.junit.Test;
 
 public class TestReferenceHolder {
+	
+	@Test
+	public void preventGarbageCollectionForLifeOfClassLoaderMaintainsInstance() {
+		Object object = new Object();
+		WeakReference<Object> ref = new WeakReference<Object>(object);
+		
+		ReferenceHolder.preventGarbageCollectionForLifeOfClassLoader(object);
+		object = null;
+		System.gc();
+		
+		assertNotNull(ref.get());
+	}
 
 	@Test
 	public void notInstantiable() throws Throwable {
