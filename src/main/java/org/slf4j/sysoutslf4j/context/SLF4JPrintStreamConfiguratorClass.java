29db518bc4ea68c4347f6a01cc241ea31d5a99b1
Robert <Robert@192.168.1.5>
2010-08-18 08:30:02 +0100

    Beginnning refactor to be less procedural

---
+++ src/main/java/org/slf4j/sysoutslf4j/context/SLF4JPrintStreamConfiguratorClass.java
@@ -0,0 +1,85 @@
+package org.slf4j.sysoutslf4j.context;
+
+import static org.slf4j.sysoutslf4j.context.ClassLoaderUtils.getJarURL;
+import static org.slf4j.sysoutslf4j.context.ClassLoaderUtils.getSystemClassLoader;
+import static org.slf4j.sysoutslf4j.context.ClassLoaderUtils.loadClass;
+
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.sysoutslf4j.common.ReflectionUtils;
+import org.slf4j.sysoutslf4j.system.SLF4JPrintStreamConfigurator;
+import org.slf4j.sysoutslf4j.system.SLF4JPrintStreamImpl;
+
+class SLF4JPrintStreamConfiguratorClass {
+	
+	private static final String LINE_END = System.getProperty("line.separator");
+	private static final Logger LOG = LoggerFactory.getLogger(SysOutOverSLF4J.class);
+
+	static Class<?> getSlf4jPrintStreamConfiguratorClass() {
+		Class<?> slf4jPrintStreamConfiguratorClass = getConfiguratorClassFromSLF4JPrintStreamClassLoader();
+		if (slf4jPrintStreamConfiguratorClass == null) {
+			slf4jPrintStreamConfiguratorClass = getConfiguratorClassFromSystemClassLoader();
+		}
+		if (slf4jPrintStreamConfiguratorClass == null) {
+			slf4jPrintStreamConfiguratorClass = addConfiguratorClassToSystemClassLoaderAndGet();
+		}
+		if (slf4jPrintStreamConfiguratorClass == null) {
+			slf4jPrintStreamConfiguratorClass = getConfiguratorClassFromCurrentClassLoader();
+		}
+		return slf4jPrintStreamConfiguratorClass;
+	}
+	
+	static boolean systemOutputsAreSLF4JPrintStreams() {
+		return System.out.getClass().getName().equals(SLF4JPrintStreamImpl.class.getName());
+	}
+
+	private static Class<?> getConfiguratorClassFromSLF4JPrintStreamClassLoader() {
+		final Class<?> configuratorClass;
+		if (systemOutputsAreSLF4JPrintStreams()) {
+			final ClassLoader classLoader = System.out.getClass().getClassLoader();
+			configuratorClass = loadClass(classLoader, SLF4JPrintStreamConfigurator.class);
+		} else {
+			configuratorClass = null;
+		}
+		return configuratorClass;
+	}
+
+	private static Class<?> getConfiguratorClassFromSystemClassLoader() {
+		Class<?> configuratorClass = null;
+		try {
+			configuratorClass = getSystemClassLoader().loadClass(SLF4JPrintStreamConfigurator.class.getName());
+		} catch (Exception e) {
+			LOG.debug("failed to load " + SLF4JPrintStreamConfigurator.class.getName() + " from system class loader due to " + e);
+		}
+		return configuratorClass;
+	}
+
+	private static Class<?> addConfiguratorClassToSystemClassLoaderAndGet() {
+		Class<?> configuratorClass = null;
+		try {
+			final URL jarUrl = getJarURL(SLF4JPrintStreamConfigurator.class);
+			ReflectionUtils.invokeMethod("addUrl", getSystemClassLoader(), URL.class, jarUrl);
+			configuratorClass = getSystemClassLoader().loadClass(SLF4JPrintStreamConfigurator.class.getName());
+		} catch (Exception exception) {
+			reportFailureToAvoidClassLoaderLeak(exception);
+		}
+		return configuratorClass;
+	}
+
+	private static void reportFailureToAvoidClassLoaderLeak(final Exception exception) {
+		LOG.warn("Unable to force syout-over-slf4j jar url into system class loader  and " +
+				"then load class [" + SLF4JPrintStreamConfigurator.class + "] from the system class loader." + LINE_END +
+				"Unfortunately it is not possible to set up Sysout over SLF4J on this system without introducing " +
+				"a class loader memory leak." + LINE_END +
+				"If you never need to discard the current class loader [" + Thread.currentThread().getContextClassLoader() + "] this will not be " +
+				"a problem and you can suppress this warning." + LINE_END +
+				"If you wish to avoid a class loader memory leak you can place sysout-over-slf4j.jar on the system classpath " +
+				"IN ADDITION TO (*not* instead of) the local context's classpath", exception);
+	}
+
+	private static Class<SLF4JPrintStreamConfigurator> getConfiguratorClassFromCurrentClassLoader() {
+		return SLF4JPrintStreamConfigurator.class;
+	}
+}
