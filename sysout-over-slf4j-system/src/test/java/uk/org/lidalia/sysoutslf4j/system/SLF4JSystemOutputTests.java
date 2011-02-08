9d21c442512f9363748bc911d589f2ff3809ca47
Robert Elliot <Robert@imac.local>
2011-02-08 23:36:04 +0000

    Completed SLF4JSystemOutputTests

--- sysout-over-slf4j-system/src/test/java/uk/org/lidalia/sysoutslf4j/system/SLF4JSystemOutputTests.java
+++ sysout-over-slf4j-system/src/test/java/uk/org/lidalia/sysoutslf4j/system/SLF4JSystemOutputTests.java
@@ -1,11 +1,37 @@
+/* 
+ * Copyright (c) 2009-2010 Robert Elliot
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free  of charge, to any person obtaining
+ * a  copy  of this  software  and  associated  documentation files  (the
+ * "Software"), to  deal in  the Software without  restriction, including
+ * without limitation  the rights to  use, copy, modify,  merge, publish,
+ * distribute,  sublicense, and/or sell  copies of  the Software,  and to
+ * permit persons to whom the Software  is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The  above  copyright  notice  and  this permission  notice  shall  be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+ * EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+ * MERCHANTABILITY,    FITNESS    FOR    A   PARTICULAR    PURPOSE    AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE,  ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package uk.org.lidalia.sysoutslf4j.system;
 
 import static org.powermock.api.easymock.PowerMock.createMock;
+import static org.powermock.api.easymock.PowerMock.expectNew;
 import static org.powermock.api.easymock.PowerMock.replayAll;
+import static org.easymock.EasyMock.isA;
+import static org.easymock.EasyMock.same;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -18,7 +44,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import uk.org.lidalia.sysoutslf4j.SysOutOverSLF4JTestCase;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(SLF4JPrintStream.class)
+@PrepareForTest({SLF4JPrintStream.class, SLF4JSystemOutput.class})
 public class SLF4JSystemOutputTests extends SysOutOverSLF4JTestCase {
 
 	@Test
@@ -105,8 +131,28 @@ public class SLF4JSystemOutputTests extends SysOutOverSLF4JTestCase {
 	}
 
 	@Test
-	public void registerLoggerAppenderMakesSLF4JPrintStreamAndRegistersLoggeAppenderIfOutputIsNotSLF4JPrintStream() {
-		fail();
+	public void registerLoggerAppenderMakesSLF4JPrintStreamAndRegistersLoggerAppenderIfSysOutIsNotSLF4JPrintStream() throws Exception {
+		registerLoggerAppenderMakesSLF4JPrintStreamAndRegistersLoggerAppenderIfOutputIsNotSLF4JPrintStream(
+				SystemOutput.OUT, SLF4JSystemOutput.OUT);
+	}
+
+	@Test
+	public void registerLoggerAppenderMakesSLF4JPrintStreamAndRegistersLoggerAppenderIfSysErrIsNotSLF4JPrintStream() throws Exception {
+		registerLoggerAppenderMakesSLF4JPrintStreamAndRegistersLoggerAppenderIfOutputIsNotSLF4JPrintStream(
+				SystemOutput.ERR, SLF4JSystemOutput.ERR);
+	}
+
+	private void registerLoggerAppenderMakesSLF4JPrintStreamAndRegistersLoggerAppenderIfOutputIsNotSLF4JPrintStream(
+			final SystemOutput output, final SLF4JSystemOutput slf4jOutput) throws Exception {
+		PrintStream original = output.get();
+		LoggerAppender logAppenderMock = createMock(LoggerAppender.class);
+		SLF4JPrintStream slf4jPrintStreamMock = createMock(SLF4JPrintStream.class);
+		expectNew(SLF4JPrintStream.class, same(original), isA(SLF4JPrintStreamDelegate.class)).andReturn(slf4jPrintStreamMock);
+		slf4jPrintStreamMock.registerLoggerAppender(logAppenderMock);
+		replayAll();
+
+		slf4jOutput.registerLoggerAppender(logAppenderMock);
+		assertSame(slf4jPrintStreamMock, output.get());
 	}
 
 	@Test
@@ -119,14 +165,13 @@ public class SLF4JSystemOutputTests extends SysOutOverSLF4JTestCase {
 		registerLoggerAppenderRegistersLoggerAppenderIfOutputIsSLF4JPrintStream(SystemOutput.ERR, SLF4JSystemOutput.ERR);
 	}
 
-	private void registerLoggerAppenderRegistersLoggerAppenderIfOutputIsSLF4JPrintStream(
-			SystemOutput output, SLF4JSystemOutput slf4jOutput) {
+	private void registerLoggerAppenderRegistersLoggerAppenderIfOutputIsSLF4JPrintStream(SystemOutput output, SLF4JSystemOutput slf4jOutput) {
 		SLF4JPrintStream slf4jPrintStreamMock = createMock(SLF4JPrintStream.class);
 		LoggerAppender logAppenderMock = createMock(LoggerAppender.class);
 		slf4jPrintStreamMock.registerLoggerAppender(logAppenderMock);
-		output.set(slf4jPrintStreamMock);
 		replayAll();
 		
+		output.set(slf4jPrintStreamMock);
 		slf4jOutput.registerLoggerAppender(logAppenderMock);
 	}
 
@@ -135,5 +180,24 @@ public class SLF4JSystemOutputTests extends SysOutOverSLF4JTestCase {
 		SLF4JSystemOutput.OUT.deregisterLoggerAppender();
 		SLF4JSystemOutput.ERR.deregisterLoggerAppender();
 		// Nothing happens
+	}
+
+	@Test
+	public void deregisterLoggerAppenderDeregistersAppenderIfSystemOutIsSLF4JPrintStream() {
+		deregisterLoggerAppenderDeregistersAppenderIfOutputIsSLF4JPrintStream(SystemOutput.OUT, SLF4JSystemOutput.OUT);
+	}
+
+	@Test
+	public void deregisterLoggerAppenderDeregistersAppenderIfSystemErrIsSLF4JPrintStream() {
+		deregisterLoggerAppenderDeregistersAppenderIfOutputIsSLF4JPrintStream(SystemOutput.ERR, SLF4JSystemOutput.ERR);
+	}
+
+	private void deregisterLoggerAppenderDeregistersAppenderIfOutputIsSLF4JPrintStream(SystemOutput output, SLF4JSystemOutput slf4jOutput) {
+		SLF4JPrintStream slf4jPrintStreamMock = createMock(SLF4JPrintStream.class);
+		slf4jPrintStreamMock.deregisterLoggerAppender();
+		replayAll();
+		
+		output.set(slf4jPrintStreamMock);
+		slf4jOutput.deregisterLoggerAppender();
 	}
 }
