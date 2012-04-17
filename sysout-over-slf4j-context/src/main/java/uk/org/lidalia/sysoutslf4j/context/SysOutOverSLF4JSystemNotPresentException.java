f831be7699de387a6be85af2a2ca64c27fce1dc0
Robert Elliot <rob@lidalia.org.uk>
2012-04-17 20:33:55 +0100

    Improved error message.

--- sysout-over-slf4j-context/src/main/java/uk/org/lidalia/sysoutslf4j/context/SysOutOverSLF4JSystemNotPresentException.java
+++ sysout-over-slf4j-context/src/main/java/uk/org/lidalia/sysoutslf4j/context/SysOutOverSLF4JSystemNotPresentException.java
@@ -6,8 +6,8 @@ public class SysOutOverSLF4JSystemNotPresentException extends RuntimeException {
 
     public static final String MESSAGE =
             "You do not seem to have sysout-over-slf4j-system on your classpath.  In order to redirect system outputs to slf4j " +
-            "it is necessary to add this jar to your classpath, at a point where it is visible to all classloaders and " +
-            "where the classloader that will load it will never discard it.";
+            "it is necessary to add this jar to your classpath, at a point where it is visible to all class loaders and " +
+            "where the class loader that will load it will never be discarded.";
 
     public SysOutOverSLF4JSystemNotPresentException(Throwable cause) {
         super(MESSAGE, cause);
